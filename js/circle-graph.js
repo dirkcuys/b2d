19450d42415f9d46441b1ff9e4e633be951a211b
Dirk Uys <dirkcuys@gmail.com>
2012-07-31 11:16:44 +0200

    checking at Munich airport

---
+++ js/circle-graph.js
@@ -0,0 +1,7 @@
+function draw(){
+    //canvas = $('#id-circle-graph');
+    canvas = document.getElementById('id-circle-graph');
+    context = canvas.getContext('2d');
+    context.fillStyle = "rgb(102,102,102)";
+    context.fillRect(10, 10, 380, 380);
+}
